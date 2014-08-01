f5f92904d690040d63894de17aefe24d9ec5ad6e
Xiaohubb <xiaohubb@hotmail.com>
2014-08-01 16:09:07 +0800

    wechat

--- frontend/views/site/notice.php
+++ frontend/views/site/notice.php
@@ -5,13 +5,13 @@
  * Date: 8/1/2014
  * Time: 1:09 PM
  */
-$this->registerCssFile(Yii::$app->basePath.'/css/layout.css');
-$this->registerCssFile(Yii::$app->basePath.'/css/common.css');
+$this->registerCssFile(Yii::$app->basePath.'/web/css/layout.css');
+$this->registerCssFile(Yii::$app->basePath.'/web/css/common.css');
 ?>
 <div class="jumbotron">
     <h1>恭喜您！</h1>
 
-    <p class="lead"><?= Yii::$app->basePath.'/css/layout.css' ?></p>
+    <p class="lead"><?= $message ?></p>
 
     <p><a class="btn btn-success" href="<?= Yii::$app->urlManager->createAbsoluteUrl('/account/deposit') ?>" style="margin-left: 15px">为账户充值</a><a style="margin-left: 15px" class="btn btn-success" href="<?= Yii::$app->urlManager->createAbsoluteUrl('/site/products') ?>">我要理财</a><a style="margin-left: 15px" class="btn btn-success" href="<?= Yii::$app->urlManager->createAbsoluteUrl('/account/transactions') ?>">交易记录</a></p>
 </div>
