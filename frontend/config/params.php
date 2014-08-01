a1ecac3680e6a5b82d16defc57c39c267bb108c8
Xiaohubb <xiaohubb@hotmail.com>
2014-08-02 00:18:18 +0800

    wechat

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -3,8 +3,8 @@ return [
     'adminEmail' => 'admin@example.com',
     'wechat'=>[
         'token'=>'f1582cb3ffa64bd4bdfca73d8795',
-        'appid'=>'wxd0360635820df2bf',
-        'appsecret'=>'f9ef7b02618bd259035e42cc9be6f9b6',
+        'appid'=>'wx806677715e0a9965',
+        'appsecret'=>'223c114936d52b4c29dddb59084b4092',
     ],
     'api' => [
         'cnpnr'=>[
