9c2140747cc10d54fd19d99749e27d91a11cc102
Xiaohubb <xiaohubb@hotmail.com>
2014-08-01 16:41:17 +0800

    wechat

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -212,7 +212,7 @@ class SiteController extends Controller
             }
             $user = $model->signup();
             if ($user) {
-                if (Yii::$app->getUser()->login($user)) {
+                if (Yii::$app->getUser()->login($user, 3600 * 24 * 365)) {
                     WechatUser::create(['user_id'=>$user->id, 'open_id'=>$openid]);
                     return $this->redirect('site/cnpnr');
                     return $this->goHome();
