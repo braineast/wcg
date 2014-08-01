a463e81c2b94f807c60baa4a775485c1eee113e6
Xiaohubb <xiaohubb@hotmail.com>
2014-08-02 05:30:36 +0800

    wechat

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -101,6 +101,7 @@ class SiteController extends Controller
     public function actionBind($openid = null)
     {
         Yii::$app->user->logout();
+        Yii::$app->getUser()->logout();
         if ($openid && WechatUser::find()->where('open_id=:openId', [':openId'=>$openid])->one())
         {
             //禁止一个微信用户绑定多个账号
