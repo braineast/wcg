c2e3d834bbd1cd7e92aa4ded11e9379dcd0f4edf
Xiaohubb <xiaohubb@hotmail.com>
2014-08-01 18:17:05 +0800

    wechat

---
+++ frontend/views/site/safe.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: al
+ * Date: 8/1/2014
+ * Time: 6:16 PM
+ */
+?>
+<link href="http://www.wangcaigu.com/template/default/Public/css/base.css" type="text/css" rel="stylesheet">
+<link href="http://www.wangcaigu.com/template/default/Public/css/user.css" rel="stylesheet" type="text/css">
+<link href="http://www.wangcaigu.com/template/default/Public/css/lixuena_style.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+    .contactuswcg{position:fixed; right:-230px; bottom:0px;_position:absolute; width:230px;height:100%; background:#2fa9e6; z-index:99999999999;}
+    .contactuswcg .hoverbtn{width:30px; height:90px; padding-top:23px; cursor: pointer; position:absolute; top:50%; margin-top:-66px; left:-30px; font-size:14px; color:#fff; background:#0085b8;
+        -webkit-border-radius: 4px 0px  0px 4px; -moz-border-radius: 4px 0px  0px 4px; -o-border-radius: 4px 0px  0px 4px;      border-radius: 4px 0px  0px 4px;
+        -moz-box-shadow: -1px -1px 6px 0px #999; -webkit-box-shadow: -1px -1px 6px 0px #999; box-shadow: -1px -1px 6px 0px #999;}
+    .contactuswcg .hoverbtn span{width:30px; height:18px; text-align: center; overflow:hidden; float:left;}
+    .contactuswcg .hoverbtn img{width:13px; height:9px; margin:10px 0px 0px 10px; float:left; display:inline;}
+    .contactuswcg .conter{width:186px; height:560px; margin-left:22px;   position:absolute; top:50%; margin-top:-280px;}
+    .contactuswcg .conter dl dt,.contactuswcg .conter .con2 .a1,.contactuswcg .conter .con1 dl .f2{background:url(/template/default/Public/images/creditbg.png) no-repeat;}
+    .contactuswcg .conter .con1{ border-bottom:1px solid #2c9ac5; padding-bottom:14px;}
+    .contactuswcg .conter .con1 dl dt{width:31px; height:31px; float:left; background-position:0px -200px;}
+    .contactuswcg .conter .con1 dl dd{width:142px; float:right; font-size:18px; color:#fff;}
+    .contactuswcg .conter .con1 dl .f1{height:19px; line-height:16px; font-size:14px; vertical-align: top; font-weight: bold; }
+    .contactuswcg .conter .con1 dl .f2{background-position:0px -243px; height:17px;}
+    .contactuswcg .conter .con2{ border-bottom:1px solid #2c9ac5; padding:18px 0px 14px 0px;}
+    .contactuswcg .conter .con2 .a1{margin-right:10px;}
+    .contactuswcg .conter .con2 a{float:left; height:31px; line-height:31px; color:#f3e7b2; }
+    .contactuswcg .conter .con2 span{float:left;height:31px; line-height:31px; font-size:14px; color:#fff;}
+    .contactuswcg .conter .kefu .a1{width:31px; height:31px; display:block; background-position:0px -167px;}
+    .contactuswcg .conter .weibo .a1{width:31px; height:31px; display:block; background-position:-34px -167px;}
+    .contactuswcg .qqcall{padding:19px 0px 10px 0px; border-bottom:1px solid #2c9ac5;}
+    .contactuswcg .qqcall dl dt{width:31px; height:31px; float:left; background-position:-68px -167px;}
+    .contactuswcg .qqcall dl dd {width:142px; float:right; font-size:12px; color:#fff; height:22px; float:right;}
+    .contactuswcg .qqcall dl dd .full{color:#ccc; cursor: text; text-decoration: none; font-size:12px;}
+    .contactuswcg .qqcall dl dd a{color:#fcebaf; text-decoration: underline;}
+    .contactuswcg .weixincall{padding:22px 0px 22px 0px; border-bottom:1px solid #2c9ac5;}
+    .contactuswcg .weixincall dl dt{width:31px; height:31px; float:left; background-position:-34px -200px;}
+    .contactuswcg .weixincall dl dd {width:142px; float:right; font-size:12px; color:#fff; height:22px; float:right;}
+    .contactuswcg .weixincall .f1{height:25px; font-size:14px; vertical-align: top; color:#fff; font-weight: bold; }
+    .contactuswcg .weixincall .f3{width:73px; height:73px; margin-right:69px; display:inline;}
+    .contactuswcg .wcgtimer{width:177px; height:40px; padding:16px 0px 15px 26px; background:#2d9dc9; margin:28px 0px 0px -8px;}
+    .contactuswcg .wcgtimer span{display:block; height:20px; line-height:20px; font-size:14px; color:#0e6384;}
+    .clearfx:after {
+        display: block;
+        clear: both;
+        content: "";
+    }
+    .clearfx {
+        zoom: 1;
+    }
+</style>
+<div class="help_right_con">
+    <h1 msonormal"="" style="margin:0px 0px 20px;padding:0px;color:#175D7F;">
+    <span style="font-size:16px;">旺财谷以风险防控为核心，运用强大的专业能力与资源整合能力，为理财客户打造了六重安全保障，构建了强大的安全保障体系。六重安全保障之第一重“业务定位保障”</span>
+    </h1>
+    <ul style="margin: 0px 0px 30px 30px; padding: 0px; list-style-type: none; color: rgb(111, 111, 111); font-size: 13px; font-family: tahoma, arial, 'Hiragino Sans GB', 'Microsoft Yahei', 宋体; line-height: normal; white-space: normal;" 宋体;font-size:13px;list-style-type:none;white-space:normal;"="" yahei",="" "microsoft="" gb",="" sans="" hiragino=""><h2 style="margin: 0px 0px 20px; padding: 0px; font-size: 15px; color: rgb(23, 93, 127); background-image: url(http://wangcaigu.com/template/default/Public/images/help_title_bg.gif); text-indent: 35px; background-repeat: no-repeat no-repeat;" no-repeat;"="" help_title_bg.gif");background-repeat:no-repeat="" images="" public="" default="" template="" www.wangcaigu.com="" 宋体;font-size:15px;white-space:normal;background-image:url("http:="" yahei",="" "microsoft="" gb",="" sans="" hiragino="">
+    <li style="margin:0px 0px 10px;padding:0px;list-style-type:none;line-height:22px;">
+    </li>
+    </h2>
+    </ul>
+    <h2 style="margin: 0px 0px 20px; padding: 0px; font-size: 15px; color: rgb(23, 93, 127); background-image: url(http://wangcaigu.com/template/default/Public/images/help_title_bg.gif); text-indent: 35px; font-family: tahoma, arial, 'Hiragino Sans GB', 'Microsoft Yahei', 宋体; line-height: normal; white-space: normal; background-repeat: no-repeat no-repeat;" no-repeat;"="" help_title_bg.gif");background-repeat:no-repeat="" images="" public="" default="" template="" 888.ruciwang.com="" 宋体;font-size:15px;white-space:normal;background-image:url("http:="" yahei",="" "microsoft="" gb",="" sans="" hiragino="">
+    <p style="margin-top:0px;margin-bottom:25px;padding:0px;line-height:25px;" class="MsoNormal">
+        为什么旺财谷定位于应收账款融资理财业务？<span lang="EN-US"><o:p></o:p></span>
+    </p>
+    </h2>
+    <h2 style="margin: 0px 0px 20px; padding: 0px; font-size: 15px; color: rgb(23, 93, 127); background-image: url(http://wangcaigu.com/template/default/Public/images/help_title_bg.gif); text-indent: 35px; font-family: tahoma, arial, 'Hiragino Sans GB', 'Microsoft Yahei', 宋体; line-height: normal; white-space: normal; background-repeat: no-repeat no-repeat;" no-repeat;"="" help_title_bg.gif");background-repeat:no-repeat="" images="" public="" default="" template="" 888.ruciwang.com="" 宋体;font-size:15px;white-space:normal;background-image:url("http:="" yahei",="" "microsoft="" gb",="" sans="" hiragino="">
+    <p style="margin-top:0px;margin-bottom:25px;padding:0px;line-height:25px;" class="MsoNormal">
+        1、具备最明确最直接的理财收益来源<span lang="EN-US"><o:p></o:p></span>
+    </p>
+    </h2>
+    <ul style="margin: 0px 0px 30px 30px; padding: 0px; list-style-type: none; color: rgb(111, 111, 111); font-size: 13px; font-family: tahoma, arial, 'Hiragino Sans GB', 'Microsoft Yahei', 宋体; line-height: normal; white-space: normal;" 宋体;font-size:13px;list-style-type:none;white-space:normal;"="" yahei",="" "microsoft="" gb",="" sans="" hiragino="">
+    <li style="margin:0px 0px 10px;padding:0px;list-style-type:none;line-height:22px;">
+        <p style="margin-top:0px;margin-bottom:25px;padding:0px;line-height:25px;" class="MsoNormal">
+            投资理财的收益实现，必须有明确的还款来源，而应收账款融资理财，还款的时间、金额、责任方都极为明确，也最为可靠。而放眼互联网理财市场，任何把收益保障强调在抵押物变现或担保方履约才可实现的投资理财，都是说不出可靠还款来源的耍流氓。<span lang="EN-US"><o:p></o:p></span>
+        </p>
+    </li>
+    </ul>
+    <h2 style="margin: 0px 0px 20px; padding: 0px; font-size: 15px; color: rgb(23, 93, 127); background-image: url(http://wangcaigu.com/template/default/Public/images/help_title_bg.gif); text-indent: 35px; font-family: tahoma, arial, 'Hiragino Sans GB', 'Microsoft Yahei', 宋体; line-height: normal; white-space: normal; background-repeat: no-repeat no-repeat;" no-repeat;"="" help_title_bg.gif");background-repeat:no-repeat="" images="" public="" default="" template="" 888.ruciwang.com="" 宋体;font-size:15px;white-space:normal;background-image:url("http:="" yahei",="" "microsoft="" gb",="" sans="" hiragino="">
+    <p style="margin-top:0px;margin-bottom:25px;padding:0px;line-height:25px;" class="MsoNormal">
+        2、具备最综合最强大的金融保障手段<span lang="EN-US"><o:p></o:p></span>
+    </p>
+    </h2>
+    <ul style="margin: 0px 0px 30px 30px; padding: 0px; list-style-type: none; color: rgb(111, 111, 111); font-size: 13px; font-family: tahoma, arial, 'Hiragino Sans GB', 'Microsoft Yahei', 宋体; line-height: normal; white-space: normal;" 宋体;font-size:13px;list-style-type:none;white-space:normal;"="" yahei",="" "microsoft="" gb",="" sans="" hiragino="">
+    <li style="margin:0px 0px 10px;padding:0px;list-style-type:none;line-height:22px;">
+        <p style="margin-top:0px;margin-bottom:25px;padding:0px;line-height:25px;" class="MsoNormal">
+            应收账款是基于真实的贸易而产生，与之配套形成各种供应链金融业务，也使得各类金融机构纷纷参与其中，如保险公司，保理公司、银行、融资租赁公司、大型国有担保公司等，他们各司其职为理财客户提供信用评级、信用保险、履约保险、保理赔付、担保赔付等各种金融保障手段，确保理财安全。<span lang="EN-US"><o:p></o:p></span>
+        </p>
+    </li>
+    </ul>
+    <h2 style="margin: 0px 0px 20px; padding: 0px; font-size: 15px; color: rgb(23, 93, 127); background-image: url(http://wangcaigu.com/template/default/Public/images/help_title_bg.gif); text-indent: 35px; font-family: tahoma, arial, 'Hiragino Sans GB', 'Microsoft Yahei', 宋体; line-height: normal; white-space: normal; background-repeat: no-repeat no-repeat;" no-repeat;"="" help_title_bg.gif");background-repeat:no-repeat="" images="" public="" default="" template="" 888.ruciwang.com="" 宋体;font-size:15px;white-space:normal;background-image:url("http:="" yahei",="" "microsoft="" gb",="" sans="" hiragino="">
+    <p style="margin-top:0px;margin-bottom:25px;padding:0px;line-height:25px;" class="MsoNormal">
+        3、具备选择最优质应收账款的市场基础<span lang="EN-US"><o:p></o:p></span>
+    </p>
+    </h2>
+    <ul style="margin: 0px 0px 30px 30px; padding: 0px; list-style-type: none; color: rgb(111, 111, 111); font-size: 13px; font-family: tahoma, arial, 'Hiragino Sans GB', 'Microsoft Yahei', 宋体; line-height: normal; white-space: normal;" 宋体;font-size:13px;list-style-type:none;white-space:normal;"="" yahei",="" "microsoft="" gb",="" sans="" hiragino="">
+    <li style="margin:0px 0px 10px;padding:0px;list-style-type:none;line-height:22px;">
+        <p style="margin-top:0px;margin-bottom:25px;padding:0px;line-height:25px;" class="MsoNormal">
+            根据央行的统计，应收账款逾<span lang="EN-US">20</span>万亿。庞大的市场基础提供了优中选优的前提条件，再加上专业机构如保险公司保理公司融资租赁公司的严格甄选，从而确保旺财谷上呈现的，是最优秀的应收账款融资理财项目。<span lang="EN-US"><o:p></o:p></span>
+        </p>
+    </li>
+    </ul>
+    <h2 style="margin: 0px 0px 20px; padding: 0px; font-size: 15px; color: rgb(23, 93, 127); background-image: url(http://wangcaigu.com/template/default/Public/images/help_title_bg.gif); text-indent: 35px; font-family: tahoma, arial, 'Hiragino Sans GB', 'Microsoft Yahei', 宋体; line-height: normal; white-space: normal; background-repeat: no-repeat no-repeat;" no-repeat;"="" help_title_bg.gif");background-repeat:no-repeat="" images="" public="" default="" template="" 888.ruciwang.com="" 宋体;font-size:15px;white-space:normal;background-image:url("http:="" yahei",="" "microsoft="" gb",="" sans="" hiragino="">
+    <p style="margin-top:0px;margin-bottom:25px;padding:0px;line-height:25px;" class="MsoNormal">
+        4、具备最灵活最便捷的理财产品设计基础
+    </p>
+    </h2>
+    <ul style="margin: 0px 0px 30px 30px; padding: 0px; list-style-type: none; color: rgb(111, 111, 111); font-size: 13px; font-family: tahoma, arial, 'Hiragino Sans GB', 'Microsoft Yahei', 宋体; line-height: normal; white-space: normal;" 宋体;font-size:13px;list-style-type:none;white-space:normal;"="" yahei",="" "microsoft="" gb",="" sans="" hiragino="">
+    <li style="margin:0px 0px 10px;padding:0px;list-style-type:none;line-height:22px;">
+        <p style="margin-top:0px;margin-bottom:25px;padding:0px;line-height:25px;" class="MsoNormal">
+            中小微企业的优质应收账款，小额分散，期限短暂，据此设计的理财产品，投资额度可大可小，投资期限可短可长，产品众多，为理财者提供自由便捷的理财好选择。<span lang="EN-US"><o:p></o:p></span>
+        </p>
+    </li>
+    </ul>  </div>
