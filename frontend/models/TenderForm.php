6d1798d41ec44935fce6e58ac3d8ffd21e446be4
Xiaohubb <xiaohubb@hotmail.com>
2014-09-11 10:14:33 +0800

    for newbieBiding support

--- frontend/models/TenderForm.php
+++ frontend/models/TenderForm.php
@@ -24,7 +24,7 @@ class TenderForm extends Model
     {
         return [
             [['dealId','amount'], 'required', 'message'=>Yii::t('tender', 'The tender amount is required.')],
-            ['amount', 'number', 'min'=>1000.00],
+//            ['amount', 'number', 'min'=>1000.00],
             ['amount', 'checkAmount'],
         ];
     }
