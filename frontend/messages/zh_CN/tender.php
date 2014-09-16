61a1e161e7b54c3777bd9cb714a0e3b879464ab3
Xiaohubb <xiaohubb@hotmail.com>
2014-09-16 17:16:58 +0800

    修改新手标功能

--- frontend/messages/zh_CN/tender.php
+++ frontend/messages/zh_CN/tender.php
@@ -9,7 +9,7 @@
 return [
     'Deal id' => '标的id',
     'Amount' => '投资金额',
-    'The tender amount is required.'=>'请您正确填写投资金额。',
+    'The tender amount is required.'=>'投标金额不允许为空，请填写投资金额。',
     'The tender amount must be more than 100.00.'=>'投资金额不能少于100元。',
     '{attribute} must be a number.' => '{attribute}必须为一个有效的数字。',
     'Username'=>'用户名',
