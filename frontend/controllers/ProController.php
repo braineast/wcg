67b630ee9cef285720c1b54a7b7a83919dace4da
Xiaohubb <xiaohubb@hotmail.com>
2014-10-22 02:13:00 -0400

    add promotion function, first user registration

---
+++ frontend/controllers/ProController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: al
+ * Date: 10/22/2014
+ * Time: 12:52 PM
+ */
+
+namespace frontend\controllers;
+
+
+use yii\web\Controller;
+
+class ProController extends Controller
+{
+    public function actionSignup()
+    {
+        $model = new SignupForm();
+        if ($model->load(Yii::$app->request->post())) {
+            if (Yii::$app->request->isAjax) {
+                Yii::$app->response->format = Response::FORMAT_JSON;
+                return ActiveForm::validate($model);
+            }
+            $user = $model->signup();
+            if ($user) {
+                if (Yii::$app->getUser()->login($user, 3600 * 24 * 365 * 10)) {
+                    return $this->redirect('site/cnpnr');
+                }
+            }
+        }
+
+        $this->layout = 'wcg';
+        return $this->render('/user/signup', ['model'=>$model]);
+    }
+}
---
+++ frontend/models/promotion/SignupForm.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: al
+ * Date: 10/22/2014
+ * Time: 1:11 PM
+ */
+
+namespace frontend\models\promotion;
+
+
+use Yii;
+use frontend\models\SignupForm as CommonSignupForm;
+use yii\base\Exception;
+
+class SignupForm extends CommonSignupForm
+{
+    public $mobileVerifyCode;
+
+    public function rules()
+    {
+        $rules = parent::rules();
+        $rules[] = ['mobileverifycode', 'required'];
+        $rules[] = ['mobileverifycode', 'verifyCode'];
+        return $rules;
+    }
+
+    private function verifyCode()
+    {
+    }
+
+    public function actionSendmobilecode()
+    {
+        try
+        {
+            $url = sprintf("%s/sendCode/phone-%s", \Yii::$app->params['api']['wcg']['baseUrl'], $this->mobile);
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $data = curl_exec($ch);
+            curl_close($ch);
+            $data = json_decode($data, true);
+            if ($data)
+            {
+                if ($data['result'] == 0 && $data['errors']['code'] == 0)
+                {
+                    $data = $data['data'];
+                    $session = Yii::$app->session;
+                    $session->set('code', json_encode($data));
+                    $session->setTimeout(300);
+                }
+            }
+        }
+        catch(Exception $e)
+        {
+        }
+    }
+}
