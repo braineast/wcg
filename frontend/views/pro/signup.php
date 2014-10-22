a2c7ebd9cfa555832afedba4b5690acda7c51d1a
Xiaohubb <xiaohubb@hotmail.com>
2014-10-22 05:04:53 -0400

    add fetch mobile verify code js function in view file

--- frontend/views/pro/signup.php
+++ frontend/views/pro/signup.php
@@ -68,7 +68,3 @@ use yii\helpers\Html;
         }
     );
 </script>
-<script type="text/javascript">
-    var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-    document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F1cdd37fcb0912f2278a8a11381ac7321' type='text/javascript'%3E%3C/script%3E"));
-</script>
