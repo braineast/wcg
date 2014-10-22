0b8d38108aec7370db9ac5eada2725574bc20a12
Xiaohubb <xiaohubb@hotmail.com>
2014-10-22 03:57:43 -0400

    add fetch mobile verify code js function in view file

--- frontend/views/pro/signup.php
+++ frontend/views/pro/signup.php
@@ -58,8 +58,9 @@ use yii\helpers\Html;
                         }).done(function(data) {
                             if (data)
                             {
-                                $(this).attr('disabled', 'disabled');
-                                $(this).html('验证码发出');
+                                $('#fetch_mobile_verify_code').attr('disabled', 'disabled');
+                                $('#fetch_mobile_verify_code').html('验证码发出');
+                                $('#fetch_mobile_verify_code').text('验证码发出');
                             }
                             else alert("失败");
                         });
